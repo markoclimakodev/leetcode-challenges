b41e576992d6e675c67414d131e3d33e9e0acf5d
markoclimako <mkclimako@gmail.com>
2023-10-17 19:47:48 -0300

    start repository

---
+++ challenges/index.ts
@@ -0,0 +1,7 @@
+import { mergeAlternately } from './merge-strings-alternately'
+import { reverseWords } from './reverse-word-string'
+
+export {
+  mergeAlternately,
+  reverseWords
+}
---
+++ index.ts
@@ -0,0 +1,8 @@
+import { mergeAlternately, reverseWords } from './challenges';
+
+console.log('test 1',mergeAlternately('abc','pqr')) 
+console.log('test 1',reverseWords('the sky is blue')) 
+
+
+
+
